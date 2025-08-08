8232cdb1115b9893d6c3f664f076877e94c65f5a
Finlay Palmer <finlay.palmer@yahoo.com>
2025-08-08 00:24:14 -0400

    added question in video functionality

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,8 +1,9 @@
 import { LearningAppFascade } from "../model/LearningAppFascade";
 import { useNavigate } from "react-router-dom";
 import { useRef, useState, useEffect } from "react";
-import videoContainerStyles from "../stylesheets/videoContainer.module.css"
-import sidebarStyles from "../stylesheets/sidebar.module.css"
+import videoContainerStyles from "../stylesheets/videoContainer.module.css";
+import sidebarStyles from "../stylesheets/sidebar.module.css";
+import questionStyles from "../stylesheets/question.module.css";
 
 function Video() {
   const learningAppFascade = LearningAppFascade.getInstance();
@@ -10,6 +11,11 @@ function Video() {
   const videoRef = useRef(null);
   const [videoPlaying, setVideoPlaying] = useState(false);
   const [videoFileName, setVideoFileName] = useState(learningAppFascade.getCurrentLesson().getVidFileName());
+  const questions = learningAppFascade.getCurrentLesson().getQuestions();
+  console.log('questions:', questions);
+  const timestamps = questions.map(question => question.getTimeStamp());
+  const [triggered, setTriggered] = useState(new Set());
+  const [playQuestion, setPlayQuestion] = useState(null);
 
   const MoveToMainScreen = () => {
     navigate("/home");
@@ -30,20 +36,54 @@ function Video() {
     }
   }
 
+  useEffect(() => {
+    const video = videoRef.current;
+
+    if (!video) return;
+
+    const handleTimeUpdate = () => {
+      const currentTime = video.currentTime;
+
+      timestamps.forEach((time) => {
+        if (
+          currentTime >= time && 
+          !triggered.has(time)
+        ) {
+          video.pause();
+          setPlayQuestion(learningAppFascade.getCurrentLesson().getQuestions()[0]);
+          setTriggered(prev => new Set(prev).add(time));
+        }
+      });
+    };
+
+    video.addEventListener('timeupdate', handleTimeUpdate);
+
+    return () => {
+      video.removeEventListener('timeupdate', handleTimeUpdate);
+    };
+  }, [triggered]);
+
   return (
     <div className="body">
       <div className={sidebarStyles.sidebar}>
                 <button name="home_button" type="button" onClick={MoveToMainScreen}>Home</button>
                 <button name="chatbot_button" type="button" onClick={ActivateChatbot}>Chatbot</button>
       </div>
-      <div className={videoContainerStyles.videoContainer}>
-        <h2>Watch this video:</h2>
-        <video ref={videoRef} width="640" height="360">
-          <source src={videoFileName} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <button onClick={togglePlay}>{videoPlaying ? "\u23F8" : "\u25B6"}</button>
-      </div>
+        {playQuestion ?
+          <div className={videoContainerStyles.videoContainer}>
+            <div className={questionStyles.questionContainer}>
+              <p>{playQuestion.getQuestion()}</p>
+            </div>
+          </div>
+        :
+          <div className={videoContainerStyles.videoContainer}>
+            <video ref={videoRef} width="640" height="360" style={{ display: playQuestion ? "none" : "block" }}>
+            <source src={videoFileName} type="video/mp4" />
+            Your browser does not support the video tag.
+            </video>
+            <button onClick={togglePlay}>{videoPlaying ? "\u23F8" : "\u25B6"}</button>
+          </div>
+        }    
     </div>
   )
 }
